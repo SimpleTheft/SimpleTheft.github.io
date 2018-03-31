14f5c393db649d1b2cf7f866aa61b7ed3c0b5dad
SilentHarmonyYT <37558286+SilentHarmonyYT@users.noreply.github.com>
2018-03-31 17:55:17 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,48 @@
-
+function jefferiesBank(){
+  var jbtake = "$319,479,370";
+  document.getElementById("take").innerHTML = "Take = " + jbtake;
+}
+function bankOfTheWest(){
+  var botwtake = "$414,456,177";
+  document.getElementById("take").innerHTML = "Take = " + botwtake;
+}
+function theTreasureChest(){
+  var ttctake = "$832,046,509";
+  document.getElementById("take").innerHTML = "Take = " + ttctake;
+}
+function excelBank(){
+  var ebtake = "$14,306,640";
+  document.getElementById("take").innerHTML = "Take = " + ebtake;
+}
+function julietsChest(){
+  var jctake = "$13,535,156";
+  document.getElementById("take").innerHTML = "Take = " + jctake;
+}
+function bestRegister(){
+  var brtake = "$19,161,376";
+  document.getElementById("take").innerHTML = "Take = " + brtake;
+}
+function woodfortBank(){
+  var wsbtake = "$6,661,254";
+  document.getElementById("take").innerHTML = "Take = " + wsbtake;
+}
+function shoresideBankTrust(){
+  var sbttake = "$3,732,574";
+  document.getElementById("take").innerHTML = "Take = " + sbttake;
+}
+function crystaLiquor(){
+  var cltake = "$8,906";
+  document.getElementById("take").innerHTML = "Take = " + cltake;
+}
+function liquorMart(){
+  var lmtake = "$9,455";
+  document.getElementById("take").innerHTML = "Take = " + lmtake;
+}
+function nutshellWoodfort(){
+  var nwtake = "$77,335";
+  document.getElementById("take").innerHTML = "Take = " + nwtake;
+}
+function littleDrakes(){
+  var ldmtake = "$33,642";
+  document.getElementById("take").innerHTML = "Take = " + ldmtake;
+}
